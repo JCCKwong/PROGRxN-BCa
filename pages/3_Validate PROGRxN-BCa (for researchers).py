2a6383145f966839b85a8f3fc16ec84e47b6a0a1
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2024-01-17 21:56:01 -0500

    Update 3_Validate PROGRxN-BCa (for researchers).py

--- pages/3_Validate PROGRxN-BCa (for researchers).py
+++ pages/3_Validate PROGRxN-BCa (for researchers).py
@@ -20,7 +20,7 @@ st.sidebar.image('https://uofturology.ca/wp-content/themes/uofturology22/img/Uof
 
 @st.cache_data()
 def load_model():
-    model = joblib.load(r'model\PROGRxN-BCa_model.joblib')
+    model = joblib.load(r'model\model.gz')
     return model
 
 model = load_model()
