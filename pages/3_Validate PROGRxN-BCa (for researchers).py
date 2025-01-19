111ec06d68ae228f73b0b299bd222866d99d5aee
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2025-01-19 18:20:29 -0500

    Update 3_Validate PROGRxN-BCa (for researchers).py

--- pages/3_Validate PROGRxN-BCa (for researchers).py
+++ pages/3_Validate PROGRxN-BCa (for researchers).py
@@ -17,6 +17,34 @@ st.title("Validate PROGRxN-BCa (for researchers)")
 st.sidebar.image('https://uofturology.ca/wp-content/themes/uofturology22/img/UofT-Urology-logo@2x.png',
                  use_column_width=True)
 
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+
 @st.cache_data()
 def load_model():
     model = joblib.load(r'model/model.gz')
