db7a17bdb42a51bb65ce6fd94ea8bcb51e17d65d
Jethro Kwong <62458997+JCCKwong@users.noreply.github.com>
2025-02-16 21:38:34 -0500

    Update 2_About PROGRxN-BCa.py

--- pages/2_About PROGRxN-BCa.py
+++ pages/2_About PROGRxN-BCa.py
@@ -142,7 +142,8 @@ layer = pdk.Layer(
     data=df,
     get_position=["lon", "lat"],
     get_color=[255, 0, 0, 200],  # Red markers
-    get_radius=500000 / (2 ** view_state.zoom),
+    get_radius=1000,
+    radius_scale=5,
     pickable=True
 )
 
